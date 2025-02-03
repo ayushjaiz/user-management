78974aa42ea006a4eafca05e1065dda7b3a10d86
Ayush Jaiswal <ayush45aj@gmail.com>
2025-02-03 06:55:51 +0530

    redis-fix

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,6 +1,11 @@
 import Redis from "ioredis";
 import { envConfig } from "./env";
 
-const redis = new Redis(envConfig.redisUrl, { maxRetriesPerRequest: null });
+const redis = new Redis(envConfig.redisUrl, {
+    maxRetriesPerRequest: null,
+    tls: {
+        rejectUnauthorized: false
+    }
+});
 
 export { redis };
